4a19fe3ba18ef7a77670008b174bcb19d14ee1e3
Rajendra <rajendra@Solomons-MacBook-Pro.local>
2014-07-03 16:55:50 +0530

    Rough Login Page implemented

--- BugTracking/BugTracking/ACEViewController.m
+++ BugTracking/BugTracking/ACEViewController.m
@@ -10,6 +10,13 @@
 
 @interface ACEViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@property (nonatomic, copy) NSString *errorMessage;
+
+- (IBAction)doLogin:(id)sender;
+
 @end
 
 @implementation ACEViewController
@@ -24,6 +31,40 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)doLogin:(id)sender
+{
+    //Textfield validation
+    BOOL correctInput = [self validateUserInput:[[self userNameTextField] text] password:[[self passwordTextField] text]];
+    
+//    if (correctInput) {
+//        <#statements#>
+//    }
+}
+
+
+/*Validate user input*/
+- (BOOL)validateUserInput:(NSString *)email password:(NSString *)password
+{
+    BOOL validInput = YES;
+    
+    //Check if both fields are empty
+    if ([email length] == 0 && [password length] == 0)
+    {
+        self.errorMessage = @"Please enter Email and Password";
+        return validInput = NO;
+    }else if ([email length] == 0 || [password length] == 0){
+        //Check any one field is empty
+        if ([email length] == 0) {
+            self.errorMessage = @"Please enter Email Address";
+            return validInput = NO;
+        }else{
+            self.errorMessage = @"Please enter Password";
+            return validInput = NO;
+        }
+    }
+    return validInput;
 }
 
 @end
